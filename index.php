71e0b3af4e439b4beaba84e6adbc9669a74530b7
Giovanni Hernández Cruz <ghernandezc2003@alumno.ipn.mx>
2026-06-15 00:38:07 -0600

    fix: Se agregó el favicon a todas las vistas

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
 <?php
 header('Location: ./public/view/ConsultaView.php');
 exit();
+?>
+<!DOCTYPE html>
+<html lang="es" data-bs-theme="dark">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+    <link rel="icon" type="image/svg+xml" href="./public/assets/img/sra_logo.svg">
+</head>
+<body>
+    
+</body>
+</html>
